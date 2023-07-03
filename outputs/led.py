45a6c01b5679651b786c962cf7fcadb33eec6a4e
ben <ben@alarmpi>
2023-07-03 20:47:25 +0200

    fixed flask problem with led.name()

--- outputs/led.py
+++ outputs/led.py
@@ -4,28 +4,28 @@ import time
 import RPi.GPIO as GPIO
 
 class Led:
-    bcm_pin = None
-    name = None
+    _bcm_pin = None
+    _name = None
 
     def __init__(self, bcm_pin : int, name : str):
-        self.bcm_pin = bcm_pin
-        self.name = name
+        self._bcm_pin = bcm_pin
+        self._name = name
 
     def setup(self):
         assert GPIO.getmode() == GPIO.BCM, "GPIO mode should be GPIO.BCM"
 
-        GPIO.setup(self.bcm_pin, GPIO.OUT)
+        GPIO.setup(self._bcm_pin, GPIO.OUT)
 
         self.off()
 
     def on(self):
-        GPIO.output(self.bcm_pin, GPIO.HIGH)
+        GPIO.output(self._bcm_pin, GPIO.HIGH)
 
     def off(self):
-        GPIO.output(self.bcm_pin, GPIO.LOW)
+        GPIO.output(self._bcm_pin, GPIO.LOW)
 
     def isOn(self):
-        return GPIO.input(self.bcm_pin) == GPIO.HIGH
+        return GPIO.input(self._bcm_pin) == GPIO.HIGH
 
     def toggle(self):
         if self.isOn():
