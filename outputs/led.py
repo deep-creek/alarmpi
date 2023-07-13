028444e493f4cc1e14342e9a6be8b5f476a705bd
ben <ben@alarmpi>
2023-07-13 16:43:11 +0200

    Change GPIO library from RPi.GPIO to gpiozero

--- outputs/led.py
+++ outputs/led.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
 
-import time
-import RPi.GPIO as GPIO
+import gpiozero
 
-class Led:
-    _bcm_pin = None
+class Led(gpiozero.LED):
     _name = None
 
     def __init__(self, bcm_pin : int, name : str):
-        self._bcm_pin = bcm_pin
+        super().__init__(bcm_pin)
         self._name = name
+        self.setup = self.off
 
-    def setup(self):
-        assert GPIO.getmode() == GPIO.BCM, "GPIO mode should be GPIO.BCM"
-
-        GPIO.setup(self._bcm_pin, GPIO.OUT)
-
-        self.off()
-
-    def on(self):
-        GPIO.output(self._bcm_pin, GPIO.HIGH)
-
-    def off(self):
-        GPIO.output(self._bcm_pin, GPIO.LOW)
-
-    def isOn(self):
-        return GPIO.input(self._bcm_pin) == GPIO.HIGH
-
-    def toggle(self):
-        if self.isOn():
-            self.off()
-        else:
-            self.on()
+    def is_on(self) -> bool:
+        return self.is_lit
 
     def name(self):
         return self._name
--- outputs/relais.py
+++ outputs/relais.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
 
-import time
-import RPi.GPIO as GPIO
+import gpiozero
 
-class Relais:
-    _bcm_pin = None
+class Relais(gpiozero.OutputDevice):
     _name = None
 
     def __init__(self, bcm_pin : int, name : str):
-        self._bcm_pin = bcm_pin
+        super().__init__(bcm_pin, active_high=False)
+
         self._name = name
 
-    def setup(self):
-        assert GPIO.getmode() == GPIO.BCM, "GPIO mode should be GPIO.BCM"
-
-        GPIO.setup(self._bcm_pin, GPIO.OUT)
-
-        self.off()
-
-    def on(self):
-        GPIO.output(self._bcm_pin, GPIO.LOW)
-
-    def off(self):
-        GPIO.output(self._bcm_pin, GPIO.HIGH)
-
+        self.setup = self.off
+        
     def is_on(self):
-        return GPIO.input(self._bcm_pin) == GPIO.LOW
-
-    def toggle(self):
-        if self.is_on():
-            self.off()
-        else:
-            self.on()
+        return self.value == False
 
     def name(self):
         return self._name
